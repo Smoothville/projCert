c233fe7455c1fb564202670b593b48fe9f89ca26
Smoothville <Smoothville@users.noreply.github.com>
2020-07-12 19:29:58 +0100

    Update NewTest3.java

--- src/test/java/com/edu/NewTest3.java
+++ src/test/java/com/edu/NewTest3.java
@@ -35,7 +35,7 @@ public class NewTest3 {
   public void verifyAbout() {
      String check = "This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 	  	
-    driver.findElement(By.id("About Us1")).click();
+    driver.findElement(By.id("About Us")).click();
      driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
       WebElement TxtBoxContent = driver.findElement(By.id("PID-ab2-pg"));
       String result = TxtBoxContent.getText();
